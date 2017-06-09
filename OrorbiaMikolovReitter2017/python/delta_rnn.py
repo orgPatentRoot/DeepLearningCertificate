9838c8016b05fda221ba8622c3a8ccc33bdcd857
sehagler <sehagler@users.noreply.github.com>
2017-06-09 12:50:27 -0700

    added rnn definitions
    Added definitions for GRU, SCRN, and SRN

--- OrorbiaMikolovReitter2017/python/delta_rnn.py
+++ OrorbiaMikolovReitter2017/python/delta_rnn.py
@@ -20,14 +20,15 @@ from log_prob import log_prob
 class delta_rnn_graph(object):
     
     # Graph constructor
-    def __init__(self, cluster_spec, num_gpus, c_size, h_size, vocabulary_size, num_training_unfoldings,
+    def __init__(self, cell_flag, num_gpus, alpha, c_size, h_size, vocabulary_size, num_training_unfoldings,
                  num_validation_unfoldings, batch_size, optimization_frequency, clip_norm, momentum):
         
         # Input hyperparameters
+        self._alpha = alpha
         self._batch_size = batch_size
         self._c_size = c_size
+        self._cell_flag = cell_flag
         self._clip_norm = clip_norm
-        self._cluster_spec = cluster_spec
         self._h_size = h_size
         self._momentum = momentum
         self._num_gpus = num_gpus
@@ -37,14 +38,13 @@ class delta_rnn_graph(object):
         self._vocabulary_size = vocabulary_size
         
         # Derived hyperparameters
-        self._num_towers = sum(self._num_gpus)
-        self._num_worker_hosts = len(self._num_gpus)
+        self._num_towers = self._num_gpus
         
         # Graph definition
         self._graph = tf.Graph()
         with self._graph.as_default():
 
-            # LSTM parameter definitions
+            # Cell parameter definitions
             self._setup_cell_parameters_wrapper()
             
             # Training data
@@ -87,21 +87,16 @@ class delta_rnn_graph(object):
                            self._training_h_saved[tower].assign(tf.zeros([self._batch_size, self._h_size]))) \
                   for tower in range(self._num_towers) ]
             
-            # Train LSTM on training data
+            # Train cell on training data
             for i in range(self._num_training_unfoldings // self._optimization_frequency):
                 training_labels = []
                 training_outputs = []
                 for tower in range(self._num_towers):
                     training_labels.append([])
                     training_outputs.append([])
-                tower = 0
-                for worker_host in range(self._num_worker_hosts):
-                    for gpu in range(self._num_gpus[worker_host]):
-                        training_labels.append([])
-                        training_outputs.append([])
-                        training_outputs[tower], training_labels[tower] = \
-                            self._training_tower(i, tower, worker_host, gpu)
-                        tower += 1
+                for tower in range(self._num_towers):
+                    training_outputs[tower], training_labels[tower] = \
+                        self._training_tower(i, tower, tower)
                 all_training_outputs = []
                 all_training_labels = []
                 for tower in range(self._num_towers):
@@ -129,21 +124,27 @@ class delta_rnn_graph(object):
                            self._validation_h_saved[tower].assign(tf.zeros([1, self._h_size]))) \
                   for tower in range(self._num_towers) ]
 
-            # Run LSTM on validation data
+            # Run cell on validation data
             validation_outputs = []
             for tower in range(self._num_towers):
                 validation_outputs.append([])
-            tower = 0
-            for worker_host in range(self._num_worker_hosts):
-                for gpu in range(self._num_gpus[worker_host]):
-                    validation_outputs[tower] = self._validation_tower(tower, worker_host, gpu)
-                    tower += 1
+            for tower in range(self._num_towers):
+                validation_outputs[tower] = self._validation_tower(tower, tower)
             logits = validation_outputs
 
             # Validation prediction, replace with hierarchical softmax in the future
             self._validation_prediction = tf.nn.softmax(logits)
             
     # RNN cells:
+    
+    # GRU cell definition
+    def _gru_cell(self, x, c, h):
+        z = tf.sigmoid(tf.matmul(x, self._Wz) + tf.matmul(h, self._Vz) + self._bz)
+        r = tf.sigmoid(tf.matmul(x, self._Wr) + tf.matmul(h, self._Vr) + self._br)
+        c = tf.matmul(x, self._Wh) + tf.matmul(r*h, self._Vh) + self._bh
+        h = z*h + (1-z)*tf.tanh(c)
+        o = tf.nn.xw_plus_b(h, self._W, self._b)
+        return o, c, h
     
     # Peephole LSTM cell definition
     def _peephole_lstm_cell(self, x, c, h):
@@ -153,48 +154,95 @@ class delta_rnn_graph(object):
         z = tf.sigmoid(tf.matmul(x, self._Wz) + tf.matmul(h, self._Vz) + self._bz)
         c = f*c + i*z
         h = o*tf.tanh(c)
+        o = tf.nn.xw_plus_b(o, self._W, self._b)
+        return o, c, h
+    
+    # SCRN cell definition
+    def _scrn_cell(self, x, c, h):
+        h = tf.sigmoid(tf.matmul(c, self._P) + tf.matmul(x, self._A) + tf.matmul(h, self._R))
+        c = (1 - self._alpha) * tf.matmul(x, self._B) + self._alpha * c
+        o = tf.matmul(h, self._U) + tf.matmul(c, self._V) 
+        return o, c, h
+    
+    # SRN cell definition
+    def _srn_cell(self, x, c, h):
+        h = tf.sigmoid(tf.matmul(x, self._A) + tf.matmul(h, self._R))
+        o = tf.matmul(h, self._U)
         return o, c, h
     
     # RNN parameters:
     
-    # Setup LSTM cell parameters
-    def _setup_peephole_lstm_cell_parameters(self):
-        
-        # Input gate input and output tensor and bias.
-        self._Wi = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
-        self._Vi = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
-        self._bi = tf.Variable(tf.zeros([1, self._h_size]))
-        
-        # Forget gate input and output tensor and bias.
-        self._Wf = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
-        self._Vf = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
-        self._bf = tf.Variable(tf.zeros([1, self._h_size]))
-
-        # Output gate input and output tensor and bias.
-        self._Wo = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
-        self._Vo = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
-        self._bo = tf.Variable(tf.zeros([1, self._h_size]))
-
-        # Cell state update input and output tensor and bias.
+    # Setup GRU cell parameters
+    def _setup_gru_cell_parameters(self):
         self._Wz = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
         self._Vz = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
         self._bz = tf.Variable(tf.zeros([1, self._h_size]))
-
-        # Softmax weight tensor and bias.
+        self._Wr = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vr = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._br = tf.Variable(tf.zeros([1, self._h_size]))
+        self._Wh = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vh = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._bh = tf.Variable(tf.zeros([1, self._h_size]))
+        self._W = tf.Variable(tf.truncated_normal([self._h_size, self._vocabulary_size], -0.1, 0.1))
+        self._b = tf.Variable(tf.zeros([self._vocabulary_size]))
+    
+    # Setup Peephole LSTM cell parameters
+    def _setup_peephole_lstm_cell_parameters(self):
+        self._Wi = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vi = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._bi = tf.Variable(tf.zeros([1, self._h_size]))
+        self._Wf = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vf = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._bf = tf.Variable(tf.zeros([1, self._h_size]))
+        self._Wo = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vo = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._bo = tf.Variable(tf.zeros([1, self._h_size]))
+        self._Wz = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._Vz = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._bz = tf.Variable(tf.zeros([1, self._h_size]))
         self._W = tf.Variable(tf.truncated_normal([self._h_size, self._vocabulary_size], -0.1, 0.1))
         self._b = tf.Variable(tf.zeros([self._vocabulary_size]))
         
-    #
+    # Setup SCRN cell parameters
+    def _setup_scrn_cell_parameters(self):
+        self._B = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._c_size], -0.1, 0.1))
+        self._A = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._P = tf.Variable(tf.truncated_normal([self._c_size, self._h_size], -0.1, 0.1))
+        self._R = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._U = tf.Variable(tf.truncated_normal([self._h_size, self._vocabulary_size], -0.1, 0.1))
+        self._V = tf.Variable(tf.truncated_normal([self._c_size, self._vocabulary_size], -0.1, 0.1))
+        
+    # Setup SRN cell parameters
+    def _setup_srn_cell_parameters(self):
+        self._A = tf.Variable(tf.truncated_normal([self._vocabulary_size, self._h_size], -0.1, 0.1))
+        self._R = tf.Variable(tf.truncated_normal([self._h_size, self._h_size], -0.1, 0.1))
+        self._U = tf.Variable(tf.truncated_normal([self._h_size, self._vocabulary_size], -0.1, 0.1))
+        
+    # Wrapper function to set cell being used
     def _cell_wrapper(self, x, c, h):
-        o, c, h = self._peephole_lstm_cell(x, c, h)
+        if self._cell_flag == 1:
+            o, c, h = self._gru_cell(x, c, h)
+        elif self._cell_flag == 2:
+            o, c, h = self._peephole_lstm_cell(x, c, h)
+        elif self._cell_flag == 3:
+            o, c, h = self._scrn_cell(x, c, h)
+        elif self._cell_flag == 4:
+            o, c, h = self._srn_cell(x, c, h)
         return o, c, h
     
-    #
+    # Wrapper function to setup cell being used
     def _setup_cell_parameters_wrapper(self):
-        self._setup_peephole_lstm_cell_parameters()
+        if self._cell_flag == 1:
+            self._setup_gru_cell_parameters()
+        if self._cell_flag == 2:
+            self._setup_peephole_lstm_cell_parameters()
+        elif self._cell_flag == 3:
+            self._setup_scrn_cell_parameters()
+        elif self._cell_flag == 4:
+            self._setup_srn_cell_parameters()
     
     # Implements a tower to run part of a batch of training data on a GPU
-    def _training_tower(self, i, tower, worker_host, gpu):
+    def _training_tower(self, i, tower, gpu):
         
         with tf.device("/job:localhost/task:0/replica:0/gpu:%d" % gpu):
             with tf.name_scope('tower_%d' % tower) as scope:
@@ -211,7 +259,7 @@ class delta_rnn_graph(object):
                     label = self._training_data[tower][i*self._optimization_frequency + j + 1]
                     o, c, h = self._cell_wrapper(x, c, h)
                     labels.append(label)
-                    outputs.append(tf.nn.xw_plus_b(o, self._W, self._b))
+                    outputs.append(o)
 
                 # Save training state and return training outputs
                 with tf.control_dependencies([self._training_c_saved[tower].assign(c),
@@ -219,7 +267,7 @@ class delta_rnn_graph(object):
                     return outputs, labels
         
     # Implements a tower to run part of a batch of validation data on a GPU
-    def _validation_tower(self, tower, worker_host, gpu):
+    def _validation_tower(self, tower, gpu):
         
         with tf.device("/job:localhost/task:0/replica:0/gpu:%d" % gpu):
             with tf.name_scope('tower_%d' % tower) as scope:
@@ -233,7 +281,7 @@ class delta_rnn_graph(object):
                 for i in range(self._num_validation_unfoldings):
                     x = self._validation_input[tower][i]
                     o, c, h = self._cell_wrapper(x, c, h)
-                    outputs.append(tf.nn.xw_plus_b(o, self._W, self._b))
+                    outputs.append(o)
 
                 # Save validation state and return validation outputs
                 with tf.control_dependencies([self._validation_c_saved[tower].assign(c), 
